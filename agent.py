964a79e075c0706cf0086a77a31de31caca52943
Anton Pchelkin <anton.pchpch@gmail.com>
2019-05-15 09:10:43 +0300

    bugfix

--- agent.py
+++ agent.py
@@ -17,16 +17,16 @@ class DQNAgent:
         self.gamma = 0.995    # discount rate
         self.epsilon = 1.0  # exploration rate
         self.epsilon_min = 0.01
-        self.epsilon_decay = 0.999
+        self.epsilon_decay = 0.9999
         self.learning_rate = 0.001
         self.model = self._build_model()
 
     def _build_model(self):
         # Neural Net for Deep-Q learning Model
         model = Sequential()
-        model.add(Dense(16, input_dim=self.state_size, activation='relu'))
-        model.add(Dense(16, activation='relu'))
-        model.add(Dense(16, activation='relu'))
+        model.add(Dense(24, input_dim=self.state_size, activation='relu'))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(24, activation='relu'))
         model.add(Dense(self.action_size, activation='linear'))
         model.compile(loss='mse',
                       optimizer=Adam(lr=self.learning_rate))
